f9d6cd4b4849812592c4f17e0b7e1fb73b2d1251
Luktronic <luktronic@gmx.at>
2025-04-14 22:44:15 +0200

    Added Lombok, Tomlj and Slf4j
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("io.freefair.lombok") version "8.13.1"
 }
 
 group = "eu.nyarie"
@@ -11,8 +12,15 @@ repositories {
 }
 
 dependencies {
+    implementation("org.tomlj:tomlj:1.1.1")
+    implementation("org.slf4j:slf4j-api:2.0.17")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.27.2")
+
+    //This isn't even needed since Gradle's logger is being used during test runtime lmao
+//    testImplementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.24.3")
 }
 
 java {
