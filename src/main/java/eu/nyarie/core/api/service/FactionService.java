b3d28ac9419e893c63ea57c3afacc93dc7c38fc2
Luktronic <luktronic@gmx.at>
2025-05-26 22:19:40 +0200

    Updated FactionService
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- src/main/java/eu/nyarie/core/api/service/FactionService.java
+++ src/main/java/eu/nyarie/core/api/service/FactionService.java
@@ -2,11 +2,28 @@ package eu.nyarie.core.api.service;
 
 import eu.nyarie.core.api.commands.faction.FactionCharacterCommands;
 import eu.nyarie.core.api.commands.faction.FactionCommands;
+import eu.nyarie.core.api.data.faction.CreateFactionData;
 import eu.nyarie.core.api.data.faction.FactionCharacterData;
 import eu.nyarie.core.api.data.faction.FactionData;
+import eu.nyarie.core.api.engine.NyarieEngine;
+import eu.nyarie.core.api.persistence.FactionRepository;
 
-public interface FactionService extends NyarieService {
+public abstract class FactionService implements NyarieService {
 
-    FactionCommands getFaction(FactionData factionData);
-    FactionCharacterCommands getFactionCharacter(FactionCharacterData characterData);
+    protected final NyarieEngine engine;
+    protected final FactionRepository factionRepository;
+
+    public FactionService(NyarieEngine engine, FactionRepository factionRepository) {
+        this.engine = engine;
+        this.factionRepository = factionRepository;
+    }
+
+    @Override
+    public NyarieEngine getEngine() {
+        return engine;
+    }
+
+    public abstract FactionCommands createFaction(CreateFactionData createFactionData);
+    public abstract FactionCommands getFaction(FactionData factionData);
+    public abstract FactionCharacterCommands getFactionCharacter(FactionCharacterData characterData);
 }
