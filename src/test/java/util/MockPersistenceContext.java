6e3de3283f3e962aa93b0333cf6902152808d87b
Luktronic <luktronic@gmx.at>
2025-05-27 17:23:37 +0200

    Added MockPersistenceContext
    Signed-off-by: Luktronic <luktronic@gmx.at>

---
+++ src/test/java/util/MockPersistenceContext.java
@@ -0,0 +1,17 @@
+package util;
+
+import eu.nyarie.core.api.engine.EnginePersistenceContext;
+import eu.nyarie.core.api.persistence.CharacterRepository;
+import eu.nyarie.core.api.persistence.FactionRepository;
+import lombok.val;
+import org.mockito.Mockito;
+
+/// An {@link EnginePersistenceContext} containing only mocked repositories
+public class MockPersistenceContext extends EnginePersistenceContext {
+    public MockPersistenceContext() {
+        val characterRepository = Mockito.mock(CharacterRepository.class);
+        val factionRepository = Mockito.mock(FactionRepository.class);
+
+        super(characterRepository, factionRepository);
+    }
+}
