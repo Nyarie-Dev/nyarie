841287c5a8c07c7a5fe095315350737cfa11ee3b
Luktronic <luktronic@gmx.at>
2025-05-27 17:25:57 +0200

    Used MockPersistenceContext in CoreClassTest
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- src/test/java/eu/nyarie/core/CoreClassTest.java
+++ src/test/java/eu/nyarie/core/CoreClassTest.java
@@ -1,19 +1,14 @@
 package eu.nyarie.core;
 
-import eu.nyarie.core.api.engine.EnginePersistenceContext;
-import eu.nyarie.core.api.persistence.CharacterRepository;
-import eu.nyarie.core.api.persistence.FactionRepository;
 import lombok.val;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import util.MockPersistenceContext;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class CoreClassTest {
 
-    private final FactionRepository factionRepository = Mockito.mock(FactionRepository.class);
-    private final CharacterRepository characterRepository = Mockito.mock(CharacterRepository.class);
-    private final EnginePersistenceContext persistenceContext = EnginePersistenceContext.from(characterRepository, factionRepository);
+    private static final MockPersistenceContext persistenceContext = new MockPersistenceContext();
 
     @Test
     void shouldCorrectlyAccessApiClasses() {
