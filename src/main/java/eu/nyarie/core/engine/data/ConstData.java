9c86acaf73b252d7040344ca850f037d02fc9ec6
Luktronic <luktronic@gmx.at>
2025-05-29 00:03:38 +0200

    Added Javadocs to ConstData
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- src/main/java/eu/nyarie/core/engine/data/ConstData.java
+++ src/main/java/eu/nyarie/core/engine/data/ConstData.java
@@ -2,6 +2,9 @@ package eu.nyarie.core.engine.data;
 
 import java.util.List;
 
+/// Parent class for all classes containing constant data.
+///
+/// Defines the {@link #load(List)} method, which loads the data into memory.
 abstract class ConstData<T> {
 
     /// Loads the passed items into const memory
