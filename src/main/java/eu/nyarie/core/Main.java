80bee1ea120f8bbf0c720cae3004228d4673df2a
Luktronic <luktronic@gmx.at>
2025-05-30 14:17:38 +0200

    Added reading of properties for const data path
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- src/main/java/eu/nyarie/core/Main.java
+++ src/main/java/eu/nyarie/core/Main.java
@@ -1,5 +1,6 @@
 package eu.nyarie.core;
 
+import eu.nyarie.core.data.ConstDataLoader;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 
@@ -10,18 +11,6 @@ import java.nio.file.Paths;
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            var uwu = Main.class.getProtectionDomain().getCodeSource().getLocation();
-            var path = Paths.get(uwu.toURI());
-            System.out.println("Path: " + path);
-            System.out.println("Absolute: " + path.toAbsolutePath());
-            System.out.println("URL: " + uwu);
-            System.out.println("URI: " + uwu.toURI());
-            final File file = new File(uwu.toURI().toString());
-            System.out.println("File: " + file);
-            System.out.println("File abs: " + file.getAbsolutePath());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        ConstDataLoader.loadDataFromJson();
     }
 }
--- src/main/java/eu/nyarie/core/data/ConstDataLoader.java
+++ src/main/java/eu/nyarie/core/data/ConstDataLoader.java
@@ -1,8 +1,41 @@
 package eu.nyarie.core.data;
 
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+
+import java.util.Optional;
+
+/// Class for loading the data from the const data JSON files (for example `regions.json`)
+///
+/// By default, the files will be searched inside the same directory that the jar executable is located in.
+/// The default path can be overwritten by:
+/// 1. Setting the `eu.nyarie.core.data.path` Java System property
+/// 2. Setting a `NYARIE_CORE_DATA_PATH` environment variable
+///
+/// The values must contain the **absolute path** of the directory in which the const data JSON files are located in.
+@Slf4j
 public class ConstDataLoader {
 
-    public static void loadDataFromJson() {
 
+    public static void loadDataFromJson() {
+        val SYSTEM_PROPERTY_NAME = "eu.nyarie.core.data.path";
+        val ENV_NAME = "NYARIE_CORE_DATA_PATH";
+        val DEFAULT_PATH = ConstDataLoader.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+
+        val systemPropertyPath = Optional.ofNullable(System.getProperty(SYSTEM_PROPERTY_NAME));
+        val envPath = Optional.ofNullable(System.getenv(ENV_NAME));
+
+        systemPropertyPath.ifPresentOrElse(
+                path -> log.info("Const data path was set using system property value: {}", path),
+                () -> envPath.ifPresentOrElse(
+                        path -> log.info("Const data path was set using environment variable value: {}", path),
+                        () -> log.info("Using default const data path: {}", DEFAULT_PATH)
+                ));
+
+        val path = systemPropertyPath
+                .orElse(envPath
+                        .orElse(DEFAULT_PATH));
+
+        System.out.println(path);
     }
 }
