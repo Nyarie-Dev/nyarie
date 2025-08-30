dfee541e4af3e58a235e41f239547f9c1c7945b9
Luktronic <luktronic@gmx.at>
2025-08-31 01:16:42 +0200

    Added basic test class for executing InstallationDirectory class
    Signed-off-by: Luktronic <luktronic@gmx.at>

---
+++ nyarie-core/src/test/java/eu/nyarie/core/io/InstallationDirectoryTest.java
@@ -0,0 +1,12 @@
+package eu.nyarie.core.io;
+
+import org.junit.jupiter.api.Test;
+
+class InstallationDirectoryTest {
+
+    @Test
+    void createInstallationDirectoryInstance() {
+        var installationDirectory = new InstallationDirectory();
+    }
+
+}
