4c08dca89b1101e77e2493c5783fdff350401b36
Luktronic <luktronic@gmx.at>
2025-08-31 23:25:28 +0200

    Moved configuring of InstallationDirectory path to its class
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- nyarie-core/src/main/java/eu/nyarie/core/io/InstallationDirectory.java
+++ nyarie-core/src/main/java/eu/nyarie/core/io/InstallationDirectory.java
@@ -1,12 +1,18 @@
 package eu.nyarie.core.io;
 
+import eu.nyarie.core.exception.data.ConstDataLoadingException;
+import eu.nyarie.core.exception.data.ConstDataNotFoundException;
+import eu.nyarie.core.io.assets.AssetsLoader;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Optional;
 
 /// Class that handles all things regarding the engine's **installation directory**
 ///
@@ -21,7 +27,7 @@ import java.util.Arrays;
 /// The values must contain the **absolute path** of the directory in which the asset JSON files are located in.
 ///
 /// ### Creation of required subdirectories
-/// 
+///
 /// After the location of the installation directory, the engine's required
 /// subdirectories, if they do not already exist, will be created. These include:
 ///
@@ -37,7 +43,7 @@ public class InstallationDirectory {
         log.debug("Initializing engine's installation path");
 
         log.trace("Locating jar location");
-        this.path = Path.of(InstallationDirectory.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+        this.path = getConfiguredPath();
         log.debug("Located jar file location: {}", path);
 
         log.info("Initializing installation directory: {}", path);
@@ -86,5 +92,42 @@ public class InstallationDirectory {
     /// @return The [Path] of the `/assets` directory inside the installation directory.
     public Path getAssetsPath() {
         return path.resolve(InstallationDirectorySubpath.ASSETS.getSubpath());
+    }
+
+    private static Path getConfiguredPath() {
+        val SYSTEM_PROPERTY_NAME = "eu.nyarie.core.installation.path";
+        val ENV_NAME = "NYARIE_CORE_INSTALLATION_PATH";
+        val DEFAULT_PATH = AssetsLoader.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+
+        val systemPropertyPath = Optional.ofNullable(System.getProperty(SYSTEM_PROPERTY_NAME));
+        val envPath = Optional.ofNullable(System.getenv(ENV_NAME));
+
+        val chosenPathStr = systemPropertyPath
+                .orElse(envPath
+                        .orElse(DEFAULT_PATH));
+
+        val path = Paths.get(chosenPathStr);
+
+        systemPropertyPath.ifPresentOrElse(
+                _ -> log.info("Asset path was set using system property value: {}", path),
+                () -> envPath.ifPresentOrElse(
+                        _ -> log.info("Asset path was set using environment variable value: {}", path),
+                        () -> log.info("No configured asset path found - using default: {}", DEFAULT_PATH)
+                ));
+
+        log.debug("Checking if path exists: {}", path);
+        if(Files.notExists(path)) {
+            log.error("Directory could not be found: {}", path);
+            val fileNotFoundException = new FileNotFoundException("Directory could not be found: %s".formatted(path));
+            throw ConstDataNotFoundException.constDataDirectoryNotFound(path.toString(), fileNotFoundException);
+        }
+
+        log.debug("Checking if path is directory: {}", path);
+        if(!Files.isDirectory(path)) {
+            log.error("Configured path is no directory: {}", path);
+            throw ConstDataLoadingException.pathIsNoDirectory(path.toString());
+        }
+
+        return path;
     }
 }
--- nyarie-core/src/main/java/eu/nyarie/core/io/assets/AssetsLoader.java
+++ nyarie-core/src/main/java/eu/nyarie/core/io/assets/AssetsLoader.java
@@ -1,17 +1,12 @@
 package eu.nyarie.core.io.assets;
 
-import eu.nyarie.core.exception.data.ConstDataLoadingException;
-import eu.nyarie.core.exception.data.ConstDataNotFoundException;
 import eu.nyarie.core.io.InstallationDirectory;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 
-import java.io.FileNotFoundException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.Optional;
 
 /// Class for loading the data from the asset JSON files (for example `regions.json`)
 ///
@@ -25,7 +20,8 @@ import java.util.Optional;
 public class AssetsLoader {
 
     public static void loadDataFromJson() {
-        val path = getConfiguredPath();
+        val installationDirectory = new InstallationDirectory();
+        val path = installationDirectory.getAssetsPath();
 
         val filenameEnums = Arrays.asList(AssetsFileNames.values());
 
@@ -41,42 +37,5 @@ public class AssetsLoader {
                     }
                 });
         //TODO check if all files exist
-    }
-
-    private static Path getConfiguredPath() {
-        val SYSTEM_PROPERTY_NAME = "eu.nyarie.core.data.path";
-        val ENV_NAME = "NYARIE_CORE_DATA_PATH";
-        val DEFAULT_PATH = AssetsLoader.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-
-        val systemPropertyPath = Optional.ofNullable(System.getProperty(SYSTEM_PROPERTY_NAME));
-        val envPath = Optional.ofNullable(System.getenv(ENV_NAME));
-
-        val chosenPathStr = systemPropertyPath
-                .orElse(envPath
-                        .orElse(DEFAULT_PATH));
-
-        val path = Paths.get(chosenPathStr);
-
-        systemPropertyPath.ifPresentOrElse(
-                _ -> log.info("Asset path was set using system property value: {}", path),
-                () -> envPath.ifPresentOrElse(
-                        _ -> log.info("Asset path was set using environment variable value: {}", path),
-                        () -> log.info("No configured asset path found - using default: {}", DEFAULT_PATH)
-                ));
-
-        log.debug("Checking if path exists: {}", path);
-        if(Files.notExists(path)) {
-            log.error("Directory could not be found: {}", path);
-            val fileNotFoundException = new FileNotFoundException("Directory could not be found: %s".formatted(path));
-            throw ConstDataNotFoundException.constDataDirectoryNotFound(path.toString(), fileNotFoundException);
-        }
-
-        log.debug("Checking if path is directory: {}", path);
-        if(!Files.isDirectory(path)) {
-            log.error("Configured path is no directory: {}", path);
-            throw ConstDataLoadingException.pathIsNoDirectory(path.toString());
-        }
-
-        return path;
     }
 }
