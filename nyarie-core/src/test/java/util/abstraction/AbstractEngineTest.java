bbd74cc16bd191e2d3be2cf027e78e29455c3c0f
Luktronic <luktronic@gmx.at>
2025-09-03 23:12:42 +0200

    Moved MockitoExtension to AbstractTest
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- nyarie-core/src/test/java/util/abstraction/AbstractEngineTest.java
+++ nyarie-core/src/test/java/util/abstraction/AbstractEngineTest.java
@@ -1,8 +1,4 @@
 package util.abstraction;
 
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-@ExtendWith(MockitoExtension.class)
 public abstract class AbstractEngineTest extends AbstractTest{
 }
--- nyarie-core/src/test/java/util/abstraction/AbstractTest.java
+++ nyarie-core/src/test/java/util/abstraction/AbstractTest.java
@@ -1,7 +1,10 @@
 package util.abstraction;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 /// Base abstract class for all tests
+@ExtendWith(MockitoExtension.class)
 public abstract class AbstractTest extends Assertions {
 }
