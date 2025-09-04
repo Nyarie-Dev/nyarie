4db7751bc8183350b09fa176e5e81f223c916658
Luktronic <luktronic@gmx.at>
2025-09-04 23:25:22 +0200

    Added AssetNotFoundException for non-absolute path
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- nyarie-core/src/main/java/eu/nyarie/core/io/assets/exception/AssetNotFoundException.java
+++ nyarie-core/src/main/java/eu/nyarie/core/io/assets/exception/AssetNotFoundException.java
@@ -4,10 +4,19 @@ import java.io.FileNotFoundException;
 
 public class AssetNotFoundException extends AssetException {
 
+    private static final String CONFIGURED_PATH_NOT_ABSOLUTE = "The configured asset directory must be absolute - was: %s";
     private static final String CONFIGURED_DIRECTORY_NOT_FOUND = "The configured asset directory could not be found: %s";
 
     protected AssetNotFoundException(String message, Throwable cause) {
         super(message, cause);
+    }
+
+    protected AssetNotFoundException(String message) {
+        super(message);
+    }
+
+    public static AssetNotFoundException configuredPathNotAbsolute(String path) {
+        return new AssetNotFoundException(CONFIGURED_PATH_NOT_ABSOLUTE.formatted(path));
     }
 
     public static AssetNotFoundException assetDirectoryNotFound(String directory, FileNotFoundException cause) {
