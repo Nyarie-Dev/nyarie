5d9f17e5907c83c90f5c62cf7cf93ad78910b8ca
Luktronic <luktronic@gmx.at>
2025-09-22 23:03:33 +0200

    Added InstallationDirectoryException
    Signed-off-by: Luktronic <luktronic@gmx.at>

---
+++ nyarie-core/src/main/java/eu/nyarie/core/io/installation/exception/InstallationDirectoryException.java
@@ -0,0 +1,27 @@
+package eu.nyarie.core.io.installation.exception;
+
+import eu.nyarie.core.api.exception.NyarieException;
+
+import java.io.FileNotFoundException;
+
+public class InstallationDirectoryException extends NyarieException {
+
+    private static final String CONFIGURED_PATH_NOT_ABSOLUTE = "The configured asset directory must be absolute - was: %s";
+    private static final String CONFIGURED_DIRECTORY_NOT_FOUND = "The configured asset directory could not be found: %s";
+
+    protected InstallationDirectoryException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    protected InstallationDirectoryException(String message) {
+        super(message);
+    }
+
+    public static InstallationDirectoryException configuredPathNotAbsolute(String path) {
+        return new InstallationDirectoryException(CONFIGURED_PATH_NOT_ABSOLUTE.formatted(path));
+    }
+
+    public static InstallationDirectoryException assetDirectoryNotFound(String directory, FileNotFoundException cause) {
+        return new InstallationDirectoryException(CONFIGURED_DIRECTORY_NOT_FOUND.formatted(directory), cause);
+    }
+}
