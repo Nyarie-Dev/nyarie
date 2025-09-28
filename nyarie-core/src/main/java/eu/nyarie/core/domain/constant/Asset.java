00da883bfcec398fb313d2ff3f19ec9489db5fa4
Luktronic <luktronic@gmx.at>
2025-09-28 23:26:27 +0200

    Added Asset interface to domain
    Signed-off-by: Luktronic <luktronic@gmx.at>

---
+++ nyarie-core/src/main/java/eu/nyarie/core/domain/constant/Asset.java
@@ -0,0 +1,11 @@
+package eu.nyarie.core.domain.constant;
+
+/// Parent interface defining that a type is an Asset.
+///
+/// Assets are loaded on startup of the engine and are **immutable** during runtime.
+/// They hold data that (usually) stays constant during a running game - such as the map, or the available items.
+///
+/// However, the assets **can** be updated and reloaded, but doing so might corrupt the game state if
+/// not done with caution.
+public interface Asset {
+}
