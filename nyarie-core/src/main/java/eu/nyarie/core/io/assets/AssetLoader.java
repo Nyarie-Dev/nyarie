632090ce55250d6f49c875865bfe00e01cca9836
Luktronic <luktronic@gmx.at>
2025-09-29 20:38:57 +0200

    Added empty AssetLoader
    Signed-off-by: Luktronic <luktronic@gmx.at>

---
+++ nyarie-core/src/main/java/eu/nyarie/core/io/assets/AssetLoader.java
@@ -0,0 +1,6 @@
+package eu.nyarie.core.io.assets;
+
+/// Interface defining a method for loading a single asset file defined in [AssetPaths]
+/// and returning its result mapped to the respective class.
+public interface AssetLoader {
+}
