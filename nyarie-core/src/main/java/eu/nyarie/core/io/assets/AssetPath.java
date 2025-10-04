78192004f9a6c99003ca94a3715fe7db6bb0c118
Luktronic <luktronic@gmx.at>
2025-10-04 23:32:27 +0200

    Moved AssetPath out of path package
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- nyarie-core/src/main/java/eu/nyarie/core/io/assets/AssetPath.java
+++ nyarie-core/src/main/java/eu/nyarie/core/io/assets/AssetPath.java
@@ -1,11 +1,11 @@
-package eu.nyarie.core.io.assets.path;
+package eu.nyarie.core.io.assets;
 
 import eu.nyarie.core.util.PathDecorator;
 
 import java.nio.file.Path;
 
 /// Class defining a [Path] that is part of an asset directory.
-public class AssetPath extends PathDecorator {
+class AssetPath extends PathDecorator {
 
     AssetPath(Path path) {
         super(path);
