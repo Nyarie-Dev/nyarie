f7d754d12976766a81650552bd4fc6d29bd474a1
Luktronic <luktronic@gmx.at>
2025-10-05 22:17:59 +0200

    Renamed AssetLoader.loadFile to loadAssetFile
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- nyarie-core/src/main/java/eu/nyarie/core/io/assets/AssetLoader.java
+++ nyarie-core/src/main/java/eu/nyarie/core/io/assets/AssetLoader.java
@@ -6,11 +6,11 @@ import java.util.List;
 
 /// Interface defining a method for loading a single asset file defined in [AssetPaths]
 /// and returning its result mapped to the respective class.
-public interface AssetLoader {
+interface AssetLoader {
 
     /// Loads the assets from the passed [asset file path][AssetFilePath] and returns a list
     /// of the assets cast to their respective [Asset] class.
     /// @param assetFilePath The path to the asset file. Must be an existing file.
-    <T extends Asset> List<T> loadFile(AssetFilePath<T> assetFilePath);
+    <T extends Asset> List<T> loadAssetFile(AssetFilePath<T> assetFilePath);
 
 }
