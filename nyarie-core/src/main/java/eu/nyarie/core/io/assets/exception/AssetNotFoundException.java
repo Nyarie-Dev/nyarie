0abdfe5201bd98d6ebda0d9196092348a33aa0fc
Luktronic <luktronic@gmx.at>
2025-10-06 21:38:30 +0200

    Added AssetNotFoundException.requiredAssetNotFound
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- nyarie-core/src/main/java/eu/nyarie/core/io/assets/exception/AssetNotFoundException.java
+++ nyarie-core/src/main/java/eu/nyarie/core/io/assets/exception/AssetNotFoundException.java
@@ -1,6 +1,10 @@
 package eu.nyarie.core.io.assets.exception;
 
+import java.nio.file.Path;
+
 public class AssetNotFoundException extends AssetException {
+
+    private static final String REQUIRED_ASSET_FILE_NOT_FOUND = "Required asset file could not be found: %s";
 
     protected AssetNotFoundException(String message, Throwable cause) {
         super(message, cause);
@@ -8,5 +12,9 @@ public class AssetNotFoundException extends AssetException {
 
     protected AssetNotFoundException(String message) {
         super(message);
+    }
+    
+    public static AssetNotFoundException requiredAssetNotFound(Path path) {
+        return new AssetNotFoundException(REQUIRED_ASSET_FILE_NOT_FOUND.formatted(path));
     }
 }
