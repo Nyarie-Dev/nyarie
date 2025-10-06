1625c71317899291dfa9672d4d879b1af9b04692
Luktronic <luktronic@gmx.at>
2025-10-06 22:29:36 +0200

    First draft implementation for FileSystemAssetLoader
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- nyarie-core/src/main/java/eu/nyarie/core/io/assets/FileSystemAssetLoader.java
+++ nyarie-core/src/main/java/eu/nyarie/core/io/assets/FileSystemAssetLoader.java
@@ -1,12 +1,21 @@
 package eu.nyarie.core.io.assets;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.nyarie.core.domain.constant.Asset;
+import eu.nyarie.core.io.assets.exception.AssetNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Optional;
 
 /// Implementation for [AssetLoader] that loads asset files from the
 /// file system.
+@Slf4j
 public class FileSystemAssetLoader implements AssetLoader {
 
     private final Path basePath;
@@ -22,9 +31,23 @@ public class FileSystemAssetLoader implements AssetLoader {
         this.basePath = basePath;
     }
 
+    /// @throws AssetNotFoundException {@inheritDoc}
     @Override
-    public <T extends Asset> List<T> loadAssetFile(AssetFilePath<T> assetFilePath) {
-        
-        return List.of();
+    public <T extends Asset> Optional<List<T>> loadAssetFile(AssetFilePath<T> assetFilePath) {
+        val path = basePath.resolve(assetFilePath);
+        log.debug("Loading asset file for class '{}': {}", assetFilePath.getAssetClass().getSimpleName(), path);
+
+        if(Files.notExists(path)) {
+            log.debug("Asset file '{}' was not found, returning empty optional", path);
+            return Optional.empty();
+        }
+
+        val om = new ObjectMapper();
+        try {
+            val response = om.readValue(path.toFile(), new TypeReference<List<T>>() { });
+            return Optional.of(response);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
