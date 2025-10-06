ac998284b1adcbcfc550435ec3f74dc75b75a10f
Luktronic <luktronic@gmx.at>
2025-10-06 22:29:17 +0200

    Made return type of loadAssetFile Optional
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- nyarie-core/src/main/java/eu/nyarie/core/io/assets/AssetLoader.java
+++ nyarie-core/src/main/java/eu/nyarie/core/io/assets/AssetLoader.java
@@ -1,9 +1,9 @@
 package eu.nyarie.core.io.assets;
 
 import eu.nyarie.core.domain.constant.Asset;
-import eu.nyarie.core.io.assets.exception.AssetNotFoundException;
 
 import java.util.List;
+import java.util.Optional;
 
 /// Interface defining a method for loading a single asset file defined in [AssetPaths]
 /// and returning its result mapped to the respective class.
@@ -12,7 +12,6 @@ interface AssetLoader {
     /// Loads the assets from the passed [asset file path][AssetFilePath] and returns a list
     /// of the assets cast to their respective [Asset] class.
     /// @param assetFilePath The path to the asset file. Must be an existing file.
-    /// @throws AssetNotFoundException If the asset file could not be found.
-    <T extends Asset> List<T> loadAssetFile(AssetFilePath<T> assetFilePath);
+    <T extends Asset> Optional<List<T>> loadAssetFile(AssetFilePath<T> assetFilePath);
 
 }
