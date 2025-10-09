c39f6975db25440e980dafbfe9c2b6fe37e88a02
Luktronic <luktronic@gmx.at>
2025-10-09 21:39:03 +0200

    Added AssetLoadingException.unexpectedErrorReadingFile
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- nyarie-core/src/main/java/eu/nyarie/core/io/assets/exception/AssetLoadingException.java
+++ nyarie-core/src/main/java/eu/nyarie/core/io/assets/exception/AssetLoadingException.java
@@ -1,14 +1,24 @@
 package eu.nyarie.core.io.assets.exception;
 
+import java.nio.file.Path;
+
 public class AssetLoadingException extends AssetException {
 
     private static final String PATH_IS_NO_DIRECTORY = "The configured path is no directory: %s";
+    private static final String UNEXPECTED_ERROR_READING_FILE = "An unexpected error occurred during reading of asset file '%s': %s";
 
     protected AssetLoadingException(String message) {
         super(message);
     }
+    protected AssetLoadingException(String message, Throwable cause) {
+        super(message, cause);
+    }
 
     public static AssetLoadingException pathIsNoDirectory(String path) {
         return new AssetLoadingException(PATH_IS_NO_DIRECTORY.formatted(path));
+    }
+
+    public static AssetLoadingException unexpectedErrorReadingFile(Path path, Throwable cause) {
+        return new AssetLoadingException(UNEXPECTED_ERROR_READING_FILE.formatted(path, cause.getMessage()), cause);
     }
 }
