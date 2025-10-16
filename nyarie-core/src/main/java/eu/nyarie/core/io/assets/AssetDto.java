98e7bd103eebb974a6c424e303deea6e32156d98
Luktronic <luktronic@gmx.at>
2025-10-16 21:29:03 +0200

    Removed unnecessary comment in AssetDto
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- nyarie-core/src/main/java/eu/nyarie/core/io/assets/AssetDto.java
+++ nyarie-core/src/main/java/eu/nyarie/core/io/assets/AssetDto.java
@@ -5,11 +5,5 @@ import eu.nyarie.core.domain.constant.Asset;
 /// Parent abstract class defining that a type is a serialization DTO of an [Asset].
 ///
 /// These are the classes used for de-/serializing an [Asset].
-///
-/// Assets are loaded on startup of the engine and are **immutable** during runtime.
-/// They hold data that (usually) stays constant during a running game - such as the map, or the available items.
-///
-/// However, the assets **can** be updated and reloaded, but doing so might corrupt the game state if
-/// not done with caution.
 public abstract class AssetDto<T extends Asset> {
 }
