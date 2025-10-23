c30f974c2de6d4edf61c16d3d33b5d27125b529c
Luktronic <luktronic@gmx.at>
2025-10-23 21:02:29 +0200

    Added logging to AssetDirectoryLoader
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- nyarie-core/src/main/java/eu/nyarie/core/io/assets/loader/AssetDirectoryLoader.java
+++ nyarie-core/src/main/java/eu/nyarie/core/io/assets/loader/AssetDirectoryLoader.java
@@ -3,6 +3,7 @@ package eu.nyarie.core.io.assets.loader;
 import eu.nyarie.core.io.assets.AssetDto;
 import eu.nyarie.core.io.assets.LoadedAssetDirectory;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 
 import java.nio.file.Path;
@@ -10,6 +11,7 @@ import java.util.Optional;
 
 /// Class responsible for loading an entire `/asset` directory,
 /// either from the file system or the JVM's classpath.
+@Slf4j
 @RequiredArgsConstructor
 public class AssetDirectoryLoader {
 
@@ -17,18 +19,24 @@ public class AssetDirectoryLoader {
     private final AssetFileLoader assetFileLoader;
 
     LoadedAssetDirectory fromFileSystem() {
+        log.debug("Loading assets from directory: {}", assetDirectory);
         return loadAssetsUsingMethod(assetFileLoader::fromFileSystem);
     }
 
     LoadedAssetDirectory fromFileSystemWithClasspathFallback() {
+        log.debug("Loading assets with classpath fallback from directory: {}", assetDirectory);
         return loadAssetsUsingMethod(assetFileLoader::fromFileSystemWithClasspathFallback);
     }
 
-    private <T extends AssetDto<?>> LoadedAssetDirectory loadAssetsUsingMethod(AssetLoaderFunction loaderFunction) {
+    private LoadedAssetDirectory loadAssetsUsingMethod(AssetLoaderFunction loaderFunction) {
+        log.trace("Calling AssetFileLoader for regions");
         val regions = loaderFunction.load(AssetPaths.REGIONS).orElseThrow();
+        log.trace("Calling AssetFileLoader for terrain types");
         val terrainTypes = loaderFunction.load(AssetPaths.TERRAIN_TYPES).orElseThrow();
 
-        return new LoadedAssetDirectory(regions, terrainTypes);
+        val loadedDirectory = new LoadedAssetDirectory(regions, terrainTypes);
+        log.debug("Finished loading asset directory");
+        return loadedDirectory;
     }
 
     @FunctionalInterface
