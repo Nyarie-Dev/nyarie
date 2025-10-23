5c423d77dc4cc2f422816fbfdc26fca848026eb0
Luktronic <luktronic@gmx.at>
2025-10-23 21:54:57 +0200

    Added utility methods to LoadedAssetDirectory
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- nyarie-core/src/main/java/eu/nyarie/core/io/assets/LoadedAssetDirectory.java
+++ nyarie-core/src/main/java/eu/nyarie/core/io/assets/LoadedAssetDirectory.java
@@ -3,9 +3,27 @@ package eu.nyarie.core.io.assets;
 import eu.nyarie.core.io.assets.map.RegionAsset;
 import eu.nyarie.core.io.assets.map.TerrainTypeAsset;
 
+import java.util.List;
+
 /// Record that holds the loaded data of all the asset files.
 public record LoadedAssetDirectory(
         RegionAsset regions,
         TerrainTypeAsset terrainTypes
 ) {
+
+    /// Counts how many loaded assets there are in this directory.
+    /// @return An `int` count of loaded assets.
+    public int count() {
+        return allAssets().size();
+    }
+
+    /// Returns an unmodifiable list containing all the assets of this directory,
+    /// including ones that were not loaded.
+    /// @return An [unmodifiable List][List#of()] containing all assets, including ones that were not loaded.
+    public List<AssetDto<?>> allAssets() {
+        return List.of(
+                regions,
+                terrainTypes
+        );
+    }
 }
