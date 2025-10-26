d2154277367999fb1b18d8a43f63b3cdc72f34db
Luktronic <luktronic@gmx.at>
2025-10-26 22:04:24 +0100

    Removed old InstallationAssets class
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- nyarie-core/src/main/java/eu/nyarie/core/Main.java
+++ nyarie-core/src/main/java/eu/nyarie/core/Main.java
@@ -1,10 +1,7 @@
 package eu.nyarie.core;
 
-import eu.nyarie.core.io.assets.loader.InstallationAssets;
-
 public class Main {
 
     public static void main(String[] args) {
-        new InstallationAssets().loadDataFromJson();
     }
 }
