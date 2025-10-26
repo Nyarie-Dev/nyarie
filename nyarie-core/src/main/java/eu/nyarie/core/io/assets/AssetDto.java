36339dfdffa61d6fd2663b44e137c5c80b266bec
Luktronic <luktronic@gmx.at>
2025-10-26 22:35:53 +0100

    Addedd AssetFileDto
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- nyarie-core/src/main/java/eu/nyarie/core/io/assets/AssetDto.java
+++ nyarie-core/src/main/java/eu/nyarie/core/io/assets/AssetDto.java
@@ -6,9 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.Arrays;
-import java.util.List;
-
 /// Parent abstract class defining that a type is a serialization DTO of an [Asset].
 ///
 /// These are the classes used for de-/serializing an [Asset].
@@ -17,10 +14,4 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class AssetDto<T extends Asset> {
 
-    private List<T> data;
-
-    @SafeVarargs
-    public AssetDto(T... data) {
-        this.data = Arrays.asList(data);
-    }
 }
