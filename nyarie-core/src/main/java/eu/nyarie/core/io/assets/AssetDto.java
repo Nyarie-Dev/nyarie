70296d9cc36f0ae8b3dfd4fc0ac392f51f8c7615
Luktronic <luktronic@gmx.at>
2025-10-26 22:23:11 +0100

    Added generic data list in AssetDto
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- nyarie-core/src/main/java/eu/nyarie/core/io/assets/AssetDto.java
+++ nyarie-core/src/main/java/eu/nyarie/core/io/assets/AssetDto.java
@@ -1,9 +1,26 @@
 package eu.nyarie.core.io.assets;
 
 import eu.nyarie.core.domain.constant.Asset;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Arrays;
+import java.util.List;
 
 /// Parent abstract class defining that a type is a serialization DTO of an [Asset].
 ///
 /// These are the classes used for de-/serializing an [Asset].
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class AssetDto<T extends Asset> {
+
+    private List<T> data;
+
+    @SafeVarargs
+    public AssetDto(T... data) {
+        this.data = Arrays.asList(data);
+    }
 }
