666f162faa99ba44395ff5b0ac5ad2bbeec1366e
Luktronic <luktronic@gmx.at>
2025-11-14 23:07:55 +0100

    Added javadoc for AppDataPathResolver

--- nyarie-core/src/main/java/eu/nyarie/core/io/appdata/AppDataPathResolver.java
+++ nyarie-core/src/main/java/eu/nyarie/core/io/appdata/AppDataPathResolver.java
@@ -1,4 +1,20 @@
 package eu.nyarie.core.io.appdata;
 
+/// Helper Class responsible for determining the path of the engine's **app data directory**.
+///
+/// ### Default path
+/// The default path of the app data directory will be in a subdirectory of the user home directory.
+/// The subdirectory however depends on the OS that the engine is running on.<br>
+/// Assuming defaults user homes with a user called `john`:
+/// - Windows: `C:\Users\john\AppData\Roaming`
+/// - Linux/UNIX: `/home/john/.local/share`
+/// - macOS: `/Users/john/Library/Application Support`
+///
+/// ### Configuration
+/// The default path is customizable and can be overwritten by (higher number means higher priority):
+/// 1. Setting the `eu.nyarie.core.appdata.path` Java System property
+/// 2. Setting the `NYARIE_CORE_APPDATA_PATH` environment variable
+///
+/// The values must contain the **absolute path** of the directory that should be used.
 final class AppDataPathResolver {
 }
