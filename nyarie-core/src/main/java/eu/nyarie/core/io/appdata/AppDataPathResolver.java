73b4aa54f144c3b75f99e70eddec2a9f2d5a19dc
Luktronic <luktronic@gmx.at>
2025-11-15 22:48:03 +0100

    Added AppDataPathResolver

--- nyarie-core/src/main/java/eu/nyarie/core/io/appdata/AppDataPathResolver.java
+++ nyarie-core/src/main/java/eu/nyarie/core/io/appdata/AppDataPathResolver.java
@@ -1,5 +1,15 @@
 package eu.nyarie.core.io.appdata;
 
+import eu.luktronic.logblock.LogBlock;
+import eu.nyarie.core.io.appdata.exception.AppDataDirectoryException;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /// Helper Class responsible for determining the path of the engine's **app data directory**.
 ///
 /// ### Default path
@@ -16,5 +26,76 @@ package eu.nyarie.core.io.appdata;
 /// 2. Setting the `NYARIE_CORE_APPDATA_PATH` environment variable
 ///
 /// The values must contain the **absolute path** of the directory that should be used.
+@Slf4j
 final class AppDataPathResolver {
+
+    private static final LogBlock logBlock = LogBlock.withLogger(log);
+
+    private final AppDataPathConfigReader configReader;
+
+    public AppDataPathResolver() {
+        this(new AppDataPathConfigReader());
+    }
+
+    /// This constructor is only used for testing-purposes. Instead, use the [#AppDataPathResolver()] constructor.
+    AppDataPathResolver(AppDataPathConfigReader configReader) {
+        log.debug("Initializing new {}", AppDataPathResolver.class.getSimpleName());
+        this.configReader = configReader;
+    }
+
+    public Path determineInstallationDirectoryPath() {
+        val DEFAULT_PATH = "TODO";
+
+        val systemPropertyPath = configReader.getSystemPropertyValue();
+        val envPath = configReader.getEnvVarValue();
+
+        val chosenPathStr = systemPropertyPath
+                .orElse(envPath
+                        .orElse(DEFAULT_PATH));
+
+        val path = Paths.get(chosenPathStr);
+
+        systemPropertyPath.ifPresentOrElse(
+                _ -> log.info("App data path was set using system property value: {}", path),
+                () -> envPath.ifPresentOrElse(
+                        _ -> log.info("App data path was set using environment variable value: {}", path),
+                        () -> log.info("No custom app data path set - using default: {}", DEFAULT_PATH)
+                ));
+
+        log.debug("Checking if path is absolute");
+        if(!path.isAbsolute()) {
+            logBlock.withBorderLength(100)
+                    .error("""
+                    ERROR SETTING UP APP DATA DIRECTORY:
+                    Configured app data path must be absolute - was: {}
+                    
+                    Tip: The path must contain the root element
+                    On Unix systems (Linux/MacOS), this is '/'
+                            Example path: '/home/john/nyarie'
+                    
+                    On Windows, this is your target partition (most commonly 'C:')
+                    Windows system also require backslashes ('\\') instead of forward slashes ('/')
+                            Example path: 'C:\\Users\\john\\nyarie'
+                    """, path);
+            throw AppDataDirectoryException.configuredPathNotAbsolute(path.toString());
+        }
+
+        log.debug("Checking if path exists: {}", path);
+        if (Files.notExists(path)) {
+            log.error("Directory could not be found: {}", path);
+            val fileNotFoundException = new FileNotFoundException("Directory could not be found: %s".formatted(path));
+            throw AppDataDirectoryException.directoryNotFound(path.toString(), fileNotFoundException);
+        }
+
+        log.debug("Checking if path is directory: {}", path);
+        if (!Files.isDirectory(path)) {
+            logBlock.error("""
+                    ERROR SETTING UP INSTALLATION DIRECTORY:
+                    Configured installation path is not a directory: {}
+                    """, path);
+            throw AppDataDirectoryException.pathIsNoDirectory(path.toString());
+        }
+
+        return path;
+    }
 }
