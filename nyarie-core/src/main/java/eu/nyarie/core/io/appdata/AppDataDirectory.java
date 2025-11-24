bf492fcc34093c3dfa3079374149644073878aba
Luktronic <luktronic@gmx.at>
2025-11-24 22:52:35 +0100

    Corrected paths in docs of AppDataDirectory class
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- nyarie-core/src/main/java/eu/nyarie/core/io/appdata/AppDataDirectory.java
+++ nyarie-core/src/main/java/eu/nyarie/core/io/appdata/AppDataDirectory.java
@@ -5,9 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 /// Class representing the Engine's Application Data Directory.
 ///
 /// By default, the AppData directory is created on the following path:
-/// - Windows: `C:\Users\\<username>\AppData\Roaming`
-/// - Linux/UNIX: `/home/<username>/.local/share`
-/// - macOS: `/home/<username>/Library/Application Support`
+/// - Windows: `C:\Users\\<username>\AppData\Roaming\nyarie`
+/// - Linux/UNIX: `/home/<username>/.local/share/nyarie`
+/// - macOS: `/home/<username>/Library/Application Support/nyarie`
 ///
 /// The location of the AppData directory can be configured by (higher number means higher priority):
 /// 1. Setting the `eu.nyarie.core.appdata.path` Java System property
