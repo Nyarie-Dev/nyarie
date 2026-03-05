12bf54dd0ea31c0c84d702b4b79616176c229dd2
Luktronic <luktronic@gmx.at>
2026-03-05 22:58:03 +0100

    feat(test): add `FileSystemUtils#deleteRecursively`
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- nyarie-core/src/test/java/eu/nyarie/core/util/io/FileSystemUtils.java
+++ nyarie-core/src/test/java/eu/nyarie/core/util/io/FileSystemUtils.java
@@ -1,9 +1,16 @@
 package eu.nyarie.core.util.io;
 
+import org.jspecify.annotations.NonNull;
+
 import java.io.File;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
 /// A class containing utils for file system interactions.
 public class FileSystemUtils {
@@ -36,5 +43,35 @@ public class FileSystemUtils {
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /// Deletes a directory recursively, meaning that the directory and all its content
+    /// will be deleted.
+    ///
+    /// If the passed [Path] is a regular file and not a directory, the File will simply get deleted.
+    ///
+    /// If the passed [Path] does not exist, nothing happens.
+    public static void deleteRecursively(Path path) throws IOException {
+        if(!Files.exists(path))
+            return;
+
+        if(!Files.isDirectory(path)) {
+            Files.delete(path);
+            return;
+        }
+
+        Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+            @Override
+            public @NonNull FileVisitResult postVisitDirectory(@NonNull Path dir, IOException exc) throws IOException {
+                Files.delete(dir);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public @NonNull FileVisitResult visitFile(@NonNull Path file, @NonNull BasicFileAttributes attrs) throws IOException {
+                Files.delete(file);
+                return FileVisitResult.CONTINUE;
+            }
+        });
     }
 }
