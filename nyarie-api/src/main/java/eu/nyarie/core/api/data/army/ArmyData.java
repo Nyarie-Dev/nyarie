51da11cbb0263359b75525b2c84da1f815554b81
Luktronic <luktronic@gmx.at>
2026-03-13 21:01:40 +0100

    feat(army): empty `ArmyData`
    Signed-off-by: Luktronic <luktronic@gmx.at>

---
+++ nyarie-api/src/main/java/eu/nyarie/core/api/data/army/ArmyData.java
@@ -0,0 +1,9 @@
+package eu.nyarie.core.api.data.army;
+
+import eu.nyarie.core.api.actions.Commandable;
+import eu.nyarie.core.api.actions.Moveable;
+import eu.nyarie.core.api.data.Identifiable;
+
+public interface ArmyData extends Identifiable, CreateArmyData, Moveable, Commandable {
+
+}
