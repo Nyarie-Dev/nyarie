58f963cdf455094316919b9fbc806536e80b1167
Luktronic <luktronic@gmx.at>
2026-04-02 19:48:07 +0200

    feat(army): extend `CombatUnitData` with `Moveable` and `Commandable`
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- nyarie-api/src/main/java/eu/nyarie/core/api/data/army/unit/CombatUnitData.java
+++ nyarie-api/src/main/java/eu/nyarie/core/api/data/army/unit/CombatUnitData.java
@@ -1,11 +1,13 @@
 package eu.nyarie.core.api.data.army.unit;
 
 import eu.nyarie.core.api.actions.Commandable;
+import eu.nyarie.core.api.actions.Moveable;
 import eu.nyarie.core.api.data.Identifiable;
 
-public interface CombatUnitData extends Identifiable {
+/// Defines a unit capable of engaging in combat. A combat unit can be of a certain [type][CombatUnitTypeData],
+/// which defines its traits.
+public interface CombatUnitData extends Identifiable, Moveable, Commandable {
 
     CombatUnitTypeData getType();
     int getHealth();
-    Commandable getCommandedBy();
 }
